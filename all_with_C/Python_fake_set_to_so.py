c92245ed178dd39a9fb665251392fc6ffe8e1988
leemengwei <1099366685@qq.com>
2021-04-02 11:01:30 +0800

    up

--- all_with_C/Python_fake_set_to_so.py
+++ all_with_C/Python_fake_set_to_so.py
@@ -1,16 +1,22 @@
+# just :python -m compileall ./   #to generate pyc
+# python Python_fake_set_to_so.py build_ext  --inplace    # to generate so
+
+
 from distutils.core import setup
 from Cython.Build import cythonize
 from glob import glob
 from IPython import embed
-
-#python 'this' build_ext  --inplace
-
-pys = []
-pys += glob("*.py")
-#pys += glob("*/*.py")
+import sys
+import os
 
 
+pys = glob("*.py")
+pys = list(set(pys) - set([sys.argv[0]]))
+#pys = ['dependency_train.py']
+
+#embed()
 for py in pys:
     setup(ext_modules=cythonize([py]))
+    pass
 
 print("Done")
