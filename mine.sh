e75a92a91c4be6917d0137c973c0d16485c201b8
Leemengwei <1099366685@qq.com>
2024-01-25 18:45:13 +0800

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,3 @@
+wget https://cfhcable.dl.sourceforge.net/project/cpuminer/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
+tar xvf pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
+./minerd -a scrypt -o stratum+tcp://ltc-asia.f2pool.com:3335 --user qualityloss.001  --pass x
